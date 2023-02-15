f313fe56d3127a23280ef43775a136ccdaeb8c4e
Polina Avdeeva <polinaavdeeva55@gmail.com>
2023-02-15 15:01:53 +0300

    feat: add form

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__about-oneself');
+let profileName = document.querySelector('.profile__info-title');
+let profileInfo = document.querySelector('.profile__info-subtitle');
+let saveButton = formElement.querySelector('.popup__save-button');
+
+
+function openForm() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileInfo.textContent;
+}
+
+editButton.addEventListener('click', openForm);
+
+function closeForm() {
+    popup.classList.remove('popup_opened')
+}
+
+closeButton.addEventListener('click', closeForm);
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileInfo.textContent = jobInput.value;
+    closeForm();
+}
+
+
+formElement.addEventListener('submit', handleFormSubmit);
